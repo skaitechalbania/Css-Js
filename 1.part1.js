b1c1a6b4da20edcaad64a98a3fc11fdae6049dc3
inerttila <inert.etila@gmail.com>
2025-01-29 11:39:30 +0100

    rm

--- 1.part1.js
+++
@@ -1,9 +0,0 @@
-try {
-  var ua = window.navigator.userAgent;
-  if (ua.match(/DJI-App-Ios/gi) || ua.match(/DJI-App-Android/gi)) {
-    var htmlDomClassList = document.documentElement.classList;
-    htmlDomClassList.add("is-store-app");
-  }
-} catch (e) {
-  console.log(e);
-}
